dfc853c30b1044d14447c404e2e2d8ac877915cb
Sonam <sonurauniyar2062@gmail.com>
2025-12-19 23:05:57 +0545

    assignment completed

--- src/q12.c
+++ src/q12.c
@@ -1,1 +1,25 @@
 // Write a C program to read the contents of a text file and display them on the screen.
+#include <stdio.h>
+
+int main() 
+{
+    FILE *file;
+    char ch;
+
+    file = fopen("input.txt", "r");
+
+    if (file == NULL) 
+    {
+        printf("Error: Could not open the file.\n");
+        return 1;
+    }
+
+    while ((ch = fgetc(file)) != EOF) 
+    {
+        putchar(ch);
+    }
+
+    fclose(file);
+
+    return 0;
+}
