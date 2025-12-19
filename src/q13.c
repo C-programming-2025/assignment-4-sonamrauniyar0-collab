8a0f1707e0208bbf59b73788d8b96ba14be8ce09
Sonam <sonurauniyar2062@gmail.com>
2025-12-19 23:08:17 +0545

    assignment completed

--- src/q13.c
+++ src/q13.c
@@ -1,1 +1,37 @@
 // Write a C program that copies the contents of one file to another file.
+#include <stdio.h>
+
+int main() 
+{
+    FILE *sourceFile, *targetFile;
+    char ch;
+
+    sourceFile = fopen("source.txt", "r");
+
+    if (sourceFile == NULL) 
+    {
+        printf("Error: Could not open source file.\n");
+        return 1;
+    }
+
+    targetFile = fopen("target.txt", "w");
+
+    if (targetFile == NULL) 
+    {
+        printf("Error: Could not open target file.\n");
+        fclose(sourceFile);
+        return 1;
+    }
+
+    while ((ch = fgetc(sourceFile)) != EOF) 
+    {
+        fputc(ch, targetFile);
+    }
+
+    fclose(sourceFile);
+    fclose(targetFile);
+
+    printf("File copied successfully.\n");
+
+    return 0;
+}
